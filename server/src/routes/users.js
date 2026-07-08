fb7a6d01e5aee74f89e8aa089cf2251d3af87788
Michael Gacheru <mikachupichu@gmail.com>
2026-07-08 10:59:44 +0300

    Request validation middleware (express-validator) for all user routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
+const { updateMeRules, getUserRules } = require('../middleware/userValidators');
 const { getMe, updateMe, getUser, deleteMe } = require('../controllers/userController');
 
 router.get('/me', auth, getMe);
-router.put('/me', auth, updateMe);
+router.put('/me', auth, updateMeRules, validate, updateMe);
 router.delete('/me', auth, deleteMe);
-router.get('/:id', getUser);
+router.get('/:id', getUserRules, validate, getUser);
 
 module.exports = router;
